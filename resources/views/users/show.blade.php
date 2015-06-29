5d96d42247c9358f22720127bb17de7ea66d6d64
alexgunner <aptpta@yahoo.es>
2015-06-29 00:29:20 -0400

    Pagina de usuario terminada con estilos

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -2,41 +2,59 @@
 
 
 @section('content')
-<h1>{{$user->username}}</h1>
+<div id="fondo2">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6 col-md-offset-3">
+				<div class="panel panel-success">
+					<div class="panel-heading text-center">My Tweets</div>
+						<div class="panel-body">
+							@if(Auth::user()->username == $user->username)
 
-@if(Auth::user()->username == $user->username)
-	<div class="car"></div>
-	<form class="form-horizontal" role="form" method="POST" action="{{ url('/tweet/store') }}">
-		<input type="hidden" name="_token" value="{{ csrf_token() }}">
-		<textarea name='tweet' id="tweet" maxlength="140"></textarea>
-		<input type="submit" value="post" name="post">
-	</form>
-@endif
+								<form role="form" method="POST" action="{{ url('/tweet/store') }}">
+									<input type="hidden" name="_token" value="{{ csrf_token() }}">
+									
+									<div class="form-group">
+										<textarea name='tweet' id="tweet" maxlength="140" class="form-control" rows="3"></textarea>
+									</div>
+									<div class="car"></div>
+									<input type="submit" value="Post" name="post" class="btn btn-success">
+								</form>
+								<hr class="divider">
 
-@if(Auth::user()->username != $user->username)
-	@if(Auth::user()->is_following($user->id))
-		 <a href={{"/unfollow/".$user->id}}>Unfollow</a>
-	@else
-		<a href={{"/follow/".$user->id}}>Follow</a>
-	@endif
-@endif
+								
+							@endif
 
-	@foreach ($user->tweets as $tweet)
-		<h2>{{$tweet->user->username}} tweeted:</h2>
-		<p>{{$tweet->tweet}}</p>
-		<p>{{$tweet->created_at}}</p>
-	@endforeach
-	
-@stop
+							@if(Auth::user()->username != $user->username)
+								@if(Auth::user()->is_following($user->id))
+									 <a href={{"/unfollow/".$user->id}}>Unfollow</a>
+								@else
+									<a href={{"/follow/".$user->id}}>Follow</a>
+								@endif
+							@endif
+
+								@foreach ($user->tweets as $tweet)
+									<h3>You tweeted:</h3>
+									<p>{{$tweet->tweet}}</p>
+									<p>{{$tweet->created_at}}</p>
+									<hr class="divider">
+								@endforeach
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+</div>
+@endsection
 
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 
 <script type="text/javascript">
 	$(document).ready(function(){
-		$('.car').html('140');
+		$('.car').html('Remaining characters: 140');
 		$('#tweet').keypress(function(){
 			c= 140 - $('#tweet').val().length;
-			$('.car').html(c);
+			$('.car').html("Remaining characters: "+c);
 		});
 	});
 </script>
