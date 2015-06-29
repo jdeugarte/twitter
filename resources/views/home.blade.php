d4b47cb76c7e87b207a2a8e9560d9c06d6f9c3a9
alexgunner <aptpta@yahoo.es>
2015-06-29 00:11:01 -0400

    Pagina principal con estilos acabada

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,33 @@
 @extends('app')
-
 @section('content')
-	@foreach ($user->tweets as $tweet)
-		<h2>{{$tweet->user->username}} tweeted:</h2>
-		<p>{{$tweet->tweet}}</p>
-		<p>{{$tweet->created_at}}</p>
-	@endforeach
+<div id="fondo2">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6 col-md-offset-3">
+				<div class="panel panel-success">
+					<div class="panel-heading text-center">Newsfeed</div>
+						<div class="panel-body">
+							@foreach ($user->tweets as $tweet)
+								<h3>You tweeted:</h3>
+								<p>{{$tweet->tweet}}</p>
+								<p>{{$tweet->created_at}}</p>
+								<hr class="divider">
+							@endforeach
 
-@foreach ($user->following() as $id)
-	@foreach (App\User::find($id)->tweets as $t)
-		<h2>{{$t->user->username}} tweeted:</h2>
-		<p>{{$t->tweet}}</p>
-		<p>{{$t->created_at}}</p>
-	@endforeach
-@endforeach
+
+							@foreach ($user->following() as $id)
+								@foreach (App\User::find($id)->tweets as $t)
+									<h3><a href={{"/".$t->user->username}}> @.{{$t->user->username}}</a> tweeted:</h3>
+									<p>{{$t->tweet}}</p>
+									<p>{{$t->created_at}}</p>
+									<hr class="divider">
+								@endforeach
+							@endforeach
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
 @endsection
